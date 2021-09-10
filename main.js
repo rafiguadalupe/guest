b250308b0bff6a2cf668897c2952298ef72dcece
rafiguadalupe <83619619+rafiguadalupe@users.noreply.github.com>
2021-09-10 17:05:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
-name_of_the_student_array=[];
+name_of_the_guest_array=[];
 function submit()
 {
-var name_1=document.getElementById("name_of_the_student_1").value;
-var name_2=document.getElementById("name_of_the_student_2").value;
-var name_3=document.getElementById("name_of_the_student_3").value;
-var name_4=document.getElementById("name_of_the_student_4").value;
-
-name_of_the_student_array.push(name_1);
- name_of_the_student_array.push(name_2);
- name_of_the_student_array.push(name_3);\
-  name_of_the_student_array.push(name_4);
-   console.log(name_of_the_student_array);
-    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+var name_1=document.getElementById("name_of_the_guest_1").value;
+var name_2=document.getElementById("name_of_the_guest_2").value;
+var name_3=document.getElementById("name_of_the_guest_3").value;
+var name_4=document.getElementById("name_of_the_guest_4").value;
+name_of_the_guest_array.push(name_1);
+name_of_the_guest_array.push(name_2);
+name_of_the_guest_array.push(name_3);
+name_of_the_guest_array.push(name_4);
+    document.getElementById("display_name").innerHTML = name_of_the_guest_array;
      document.getElementById("submit_button").style.display = "none";
  document.getElementById("sort_button").style.display = "inline-block";
 
 
 }
-function sorting() { name_of_the_student_array.sort();
-     console.log(name_of_the_student_array);
-      document.getElementById("display_name").innerHTML = name_of_the_student_array; }
+function sorting() { name_of_the_guest_array.sort();
+     console.log(name_of_the_guest_array);
+      document.getElementById("display_name").innerHTML = name_of_the_guest_array; }
